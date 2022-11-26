edeed3563075c32bcf306add7f1d14f2c0981f7a
Maksim <itskovmaxim@mail.ru>
2022-11-26 21:15:16 +0300

    6

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите день недели числом от 1 до 7 включительно: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<=0 || n>7)
+Console.WriteLine("Введенное число не соответсвует условию, повторите ввод!");
+else
+{
+    if (n==6 || n==7)
+    Console.WriteLine("Выходной");
+    else
+    Console.WriteLine("Не выходной");
+}
---
+++ Task1_1/Program.cs
@@ -0,0 +1,39 @@
+﻿/* int n = new Random().Next(10,100);
+int n1 = n/10;
+int n2 = n%10;
+Console.WriteLine($"Случайное число: {n}");
+if (n1>n2)
+    Console.Write(n1);
+else
+    Console.Write(n2); 
+
+    int n = new Random().Next(100,1000);
+    int n1 = n/100;
+    int n2 = (n/10)%10;
+    int n3 = n%10;
+    Console.WriteLine($"Случайное число: {n}");
+    Console.WriteLine(n1*10+n3);
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b % a == 0)
+Console.WriteLine("Кратно");
+else
+Console.WriteLine(b%a); 
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n % 7 == 0) && (n % 23 == 0))
+Console.WriteLine("ДА");
+else
+Console.WriteLine("Нет");*/
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if ( (a == b * b) || (b == a * a))
+Console.WriteLine("Да");
+else
+Console.WriteLine("Нет");
