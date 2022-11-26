25712aeb10fb0a075709aa98d392ee6e0a24c692
Maksim <itskovmaxim@mail.ru>
2022-11-26 14:43:25 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Console.Write("Введите 1 число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m * m == n)
+    Console.Write("yes");
+else
+    Console.Write("no");
+
+Console.Write("Введите день недели:");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n<1 || n>7)
+{
+    Console.Write("Вы ошиблись, введите день:");
+    int n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1)
+Console.Write("Понедельник");
+else if (n == 2)
+Console.Write("Вторник");
+else if (n == 3)
+Console.Write("Среда");
+else if (n == 4)
+Console.Write("Четверг");
+else if (n == 5)
+Console.Write("Пятница");
+else if (n == 6)
+Console.Write("Суббота");
+else 
+Console.Write("Воскресенье");
+
+
+
+Console.Write("Введите  число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = n * (-1);
+while (i <= n)
+{
+    Console.Write($"{i} ");
+    i =i+1;
+}*/
+Console.Write("Введите  число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = n * (-1)
+for (int i + (-1)*n; i<=n;  i++)
+    Console.Write(i + " ");
---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b)
+{
+    Console.WriteLine("max = " + b);
+    Console.WriteLine("min = " + a); 
+}
+else
+{
+    Console.WriteLine("max = " + a);
+    Console.WriteLine("min = " + b);
+}
