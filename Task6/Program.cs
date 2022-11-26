ae9795efe5db6e2563ccf3accc00acfeba3d81c0
Maksim <itskovmaxim@mail.ru>
2022-11-26 14:55:17 +0300

    3

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%2 == 0)
+{
+Console.Write("Число чётное");
+}
+else 
+{
+    Console.Write("Число нечётное");
+}
