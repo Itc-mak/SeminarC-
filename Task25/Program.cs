43ca1c974923025d06709c737174f78fb779d2ff
Maksim <itskovmaxim@mail.ru>
2022-12-04 18:20:14 +0300

    25

---
+++ Task25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = a;
+int i = 0;
+for (i = 1; i <= b-1; i++)
+res = res * a;
+Console.WriteLine(res);
