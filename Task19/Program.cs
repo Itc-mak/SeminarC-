0242f86a16b74b6acb4655a75b5511d7cf9402cc
Maksim <itskovmaxim@mail.ru>
2022-12-10 21:11:12 +0300

    old

--- Task19/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Введите пятизначное число: ");
-string c = Convert.ToString(Console.ReadLine());;
-int len = c.Length;
-if (len<=4 || len>=6)
-Console.WriteLine("Ошибка, число не пятизначное. Попробуйте сначала");
-else if (c[0] == c[4] && c[1] ==c[3])
-Console.WriteLine("Палиндром ");
-else
-Console.WriteLine("не палиндром ");
--- Task25/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Введите первое число");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int b = Convert.ToInt32(Console.ReadLine());
-int res = a;
-int i = 0;
-for (i = 1; i <= b-1; i++)
-res = res * a;
-Console.WriteLine(res);
