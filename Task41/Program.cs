768a5cf72d5b79c7c59d203ddcc90a818dae4dec
Maksim <itskovmaxim@mail.ru>
2022-12-11 12:51:28 +0300

    41

---
+++ Task41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите количество цифр: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 0)
+count++;
+}
+Console.WriteLine($"Количество положительных чисел: {count}");
