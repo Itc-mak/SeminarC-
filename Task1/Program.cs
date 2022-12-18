897fa6c74de0554d20f7f9763e2587eadf706c96
Maksim <itskovmaxim@mail.ru>
2022-12-18 18:46:39 +0300

    47

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -234,6 +234,7 @@ a1 = x;
 } */
 
 //Task45
+/*
 nt[] a = {1, 2, 3, 4, 5};
 int[] b = new int[a.Length];
 
@@ -243,3 +244,42 @@ b[i] = a[i];
 b[0] = -1;
 Console.WriteLine($"[{string.Join(", ", a)}]");
 Console.WriteLine($"[{string.Join(", ", b)}]");
+*/
+
+void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+               int count = count + matrix[i,j];
+            }
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(s => int.Parse(s)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальная матрица: ");
+InputMatrix(matrix);
+Console.WriteLine("Результат: ");
+ReleaseMatrix(matrix);
---
+++ Task47/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputMatrix(double [, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble(); 
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] coord = Console.ReadLine().Split(" ");
+int row = int.Parse(coord[0]);
+int column = int.Parse(coord[1]);
+double [,] matrix = new double[row, column];
+InputMatrix(matrix);
